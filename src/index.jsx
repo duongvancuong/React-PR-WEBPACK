7c091e4b49fc9a43500144fad3ca4d155c7529b2
duongvancuong <vancuong279dn@gmail.com>
2018-08-14 11:52:17 +0700

    Update Config

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/app.scss';
 
 const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div className="container">{title}</div>,
   document.getElementById('app'),
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,135 @@
 const webpack = require('webpack');
+const merge = require("webpack-merge");
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const glob = require("glob");
 
-module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.jsx'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new webpack.WatchIgnorePlugin([
-      path.join(__dirname, "node_modules")
-    ]),
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    open: true,
-    overlay: true,
-    watchOptions: {
-      // Delay the rebuild after the first change
-      aggregateTimeout: 300,
-      // Poll using interval (in ms, accepts boolean too)
-      poll: 1000,
+const parts = require("./webpack.parts");
+
+const PATHS = {
+  app: path.join(__dirname, "src"),
+};
+
+const commonConfig = merge([
+  {
+    entry: [
+      'react-hot-loader/patch',
+      './src/index.jsx'
+    ],
+    devtool: "sourcemap",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader']
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
+            }
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader'
+            }
+          ]
+        },
+        {
+          // Match woff2 and patterns like .woff?v=1.1.1.
+          test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 50000,
+              mimetype: "application/font-woff",
+              name: "./fonts/[name].[ext]", // Output below ./fonts
+              publicPath: "../", // Take the directory into account
+            },
+          },
+        },
+      ]
     },
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.json'],
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html",
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+        inject: true,
+        trackJSToken: '',
+      }),
+      new webpack.WatchIgnorePlugin([
+        path.join(__dirname, "node_modules")
+      ]),
+    ],
+  },
+  parts.loadJavaScript({ include: PATHS.app }),
+]);
+
+const productionConfig = merge([
+  parts.generateSourceMaps({ type: "eval-source-map" }),
+  parts.purifyCSS({
+    paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
+  }),
+  parts.extractCSS({
+    use: [parts.cssLoader(), parts.scssLoader(), parts.autoprefix()],
+  }),
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: "[name].[ext]",
+    },
+  }),
+]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+  parts.extractCSS({
+    use: [parts.cssLoader(), parts.scssLoader(), parts.autoprefix()],
+  }),
+  parts.loadImages(),
+]);
+
+module.exports = mode => {
+  process.env.BABEL_ENV = mode;
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
   }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
